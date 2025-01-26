7637759de211c00f53709f122c1bc683380c6f05
keita1899 <saboten18k@gmail.com>
2025-01-26 19:09:11 +0900

    chore: タイムゾーンを日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module Myapp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = "Tokyo"
+
+    config.active_record.default_timezone = :local
   end
 end
