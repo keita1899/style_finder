15bdc48cd96fe180da283ba9a8fb38375d777c2c
keita1899 <saboten18k@gmail.com>
2025-02-03 17:22:19 +0900

    refactor: 不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :recognitions, only: %i[new create] do
-    collection do
-      post "upload"
-    end
-  end
+  resources :recognitions, only: %i[new create]
   match "*path", to: "application#not_found", via: :all
 end
